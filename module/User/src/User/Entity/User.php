f743ee2a61186a435037c93261df4ad0a44a1935
enjoy2000 <enjoy3013@gmail.com>
2014-09-19 00:35:31 +0700

    init project with error on db connect

---
+++ module/User/src/User/Entity/User.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: antiprovn
+ * Date: 9/19/14
+ * Time: 12:14 AM
+ */
+namespace User\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/** @ORM\Entity */
+class User{
+
+    /**
+     * @ORM\id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Coulumn(type="interger")
+     */
+    protected $id;
+
+    /** @ORM\Column(type="string") */
+    protected $fullName;
+
+    /**
+     * Get id
+     *
+     * @return interger
+     */
+    public function getId(){
+        return $this->id;
+    }
+
+    /**
+     *
+     * Set full name
+     * @param string $fullName
+     * @return interger
+     */
+    public function setFullname($fullName){
+        $this->fullName = $fullName;
+
+        return $this;
+    }
+}
