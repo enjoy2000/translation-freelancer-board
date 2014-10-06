ede63c6bb5d74cf288e6f63e337eb216d057f36f
enjoy2000 <enjoy3013@gmail.com>
2014-10-06 14:05:03 +0700

    Fix namespace wrong

--- module/User/src/User/Controller/LoginController.php
+++ module/User/src/User/Controller/LoginController.php
@@ -65,7 +65,7 @@ class LoginController extends AbstractActionController
         if (! $container->offsetExists('authenticated')) {
             echo 'No user logged in';
         }
-        /** @var OrgHeiglHybridAuth\UserInterface $user */
+        /** @var \OrgHeiglHybridAuth\UserInterface $user */
         $user = $container->offsetGet('user');
         echo $user->getName(); // The name of the logged in user
         echo $user->getUID();  // The internal UID of the used service
